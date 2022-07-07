5723a52c7f7cf6a85f9f640adc6cc94b2011d365
Druumihra <teis.bindner@laerke.net>
2022-07-07 09:11:02 +0200

    first commit

---
+++ My project/Assets/Player_Stats.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Stats : MonoBehaviour
+{
+
+    public float hp = 100;
+    public float movespeed = 0.0f;
+    public float physicaldefense = 1 ;
+    public float magicaldefense = 1;
+    public float absolutedefense = 0;
+    public float hpregen = 0.5f;
+    public float mana = 20;
+    public float manaregen = 0.2f;
+    public float luck = 0;
+    public float curse = 0;
+    public float attackspeed = 0.5f;
+    public float level = 1;
+    
+
+}
